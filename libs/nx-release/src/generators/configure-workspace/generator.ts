dbe072f69a4c87e432c466142a97c1a892baac45
kreuzerk <kevin.kreuzer90@icloud.com>
2023-07-10 10:33:53 +0200

    feat: 🎸 add workspace configuration generator

--- libs/nx-release/src/generators/configure-workspace/generator.ts
+++ libs/nx-release/src/generators/configure-workspace/generator.ts
@@ -1,41 +1,49 @@
-import {
-  addProjectConfiguration,
-  formatFiles,
-  generateFiles,
-  Tree,
-} from '@nx/devkit';
-import ora from 'ora';
-import chalk from 'chalk';
+import * as ora from "ora";
 import * as path from 'path';
 import {execSync} from "child_process";
+import {formatFiles, generateFiles, Tree,} from '@nx/devkit';
 
-import { ConfigureWorkspaceGeneratorSchema } from './schema';
+import {ConfigureWorkspaceGeneratorSchema} from './schema';
 
 export async function configureWorkspaceGenerator(
   tree: Tree,
   options: ConfigureWorkspaceGeneratorSchema
 ) {
+  const {installDeps, generateReleaseConfig, generateWorkflows} = options;
 
   const spinner = ora();
   try {
-    console.log(chalk.blue('🐋 nx-release: we are going to configure your workspace for release'));
-    console.log(chalk.blue('🐋 nx-release: first we will install the required deps'));
 
-    spinner.text = '🐋 nx-release: Installing dependencies';
-    spinner.start();
-    execSync(`npm i -D @semantic-release/changelog @semantic-release/commit-analyzer @semantic-release/exec @semantic-release/git @semantic-release/release-notes-generator nx-release replace-json-property`);
-    spinner.succeed();
+    if (installDeps) {
+      spinner.text = '🐋 nx-release: Installing dependencies';
+      spinner.start();
+      execSync(`npm i -D @semantic-release/changelog @semantic-release/commit-analyzer @semantic-release/exec @semantic-release/git @semantic-release/release-notes-generator nx-release replace-json-property`);
+      spinner.succeed();
+    }
 
-    spinner.text = `🐋 nx-release: generating release config`;
-    spinner.start();
-    generateFiles(tree, path.join(__dirname, 'files'), '.', options);
-    spinner.succeed();
+    if (generateReleaseConfig || generateWorkflows) {
+      const artifacts = getArtifacts(generateReleaseConfig, generateWorkflows);
+      spinner.text = `🐋 nx-release: generating ${artifacts}`;
+      spinner.start();
+      generateFiles(tree, path.join(__dirname, 'files'), '.', options);
+      spinner.succeed();
+    }
 
     await formatFiles(tree);
-  }
-  catch (e) {
+  } catch (e) {
     spinner.fail(`🐋 nx-release: something went wrong: ${e.toString()}`)
   }
+}
+
+function getArtifacts(generateReleaseConfig: boolean, generateWorkflows: boolean) {
+  let artifacts = '';
+  if (generateReleaseConfig) {
+    artifacts += 'release config';
+  }
+  if(generateWorkflows){
+    artifacts += artifacts.length > 0 ? '& GitHub actions': 'GitHub actions'
+  }
+  return artifacts;
 }
 
 export default configureWorkspaceGenerator;
--- libs/nx-release/src/generators/configure-workspace/schema.d.ts
+++ libs/nx-release/src/generators/configure-workspace/schema.d.ts
@@ -1,3 +1,5 @@
 export interface ConfigureWorkspaceGeneratorSchema {
-  name: string;
+  installDeps?: boolean;
+  generateReleaseConfig?: boolean;
+  generateWorkflows?: boolean;
 }
