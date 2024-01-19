c71f7dd000593d4ffb869a5d26db4850690adc76
canserkanuren <canserkan.uren@gmail.com>
2024-01-19 13:14:15 +0100

    feat(npm-publish): do not override current .npmrc file

--- libs/nx-release/src/executors/npm-publish/executor.ts
+++ libs/nx-release/src/executors/npm-publish/executor.ts
@@ -11,7 +11,7 @@ export default async function runExecutor(options: NpmPublishExecutorSchema,
   const sourceRoot = `./dist/${getRoot(context)}`;
   const registry: string = process.env.NPM_REGISTRY || 'registry.npmjs.org'
   execSync(
-    `cd ${sourceRoot} && echo '//${registry}/:_authToken=${process.env.NPM_TOKEN}' > .npmrc && npm publish`
+    `cd ${sourceRoot} && echo '//${registry}/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish`
   );
   return {
     success: true,
