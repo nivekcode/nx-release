8715172a939c524d2fcef37d8090b000891a096a
markuczy <martin.kuczynski@capgemini.com>
2024-11-29 11:14:46 +0100

    feat: npm publish uses distribution tags

--- libs/nx-release/src/executors/npm-publish/executor.spec.ts
+++ libs/nx-release/src/executors/npm-publish/executor.spec.ts
@@ -1,6 +1,6 @@
-import * as child_process from "child_process";
+import * as child_process from 'child_process';
 
-import * as projectHelpers from "../helpers/projects.helpers";
+import * as projectHelpers from '../helpers/projects.helpers';
 
 import executor from './executor';
 
@@ -17,15 +17,14 @@ describe('NpmPublish Executor', () => {
 
   it('should execSync with a default libPath if no libPath was provided', async () => {
     const mockRoot = 'libs/my-domain/foo';
-    const context = {
-    } as any;
+    const context = {} as any;
 
     /* eslint-disable */
     jest.spyOn(child_process, 'execSync').mockImplementation((() => {}) as any);
     /* eslint-disable */
     jest.spyOn(projectHelpers, 'getRoot').mockReturnValue(mockRoot);
 
-    const expectedCommand = `cd ./dist/${mockRoot} && echo '//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish`
+    const expectedCommand = `cd ./dist/${mockRoot} && echo '//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish --tag=latest`;
     const output = await executor({}, context);
 
     expect(child_process.execSync).toHaveBeenCalledWith(expectedCommand);
@@ -34,8 +33,7 @@ describe('NpmPublish Executor', () => {
 
   it('should execSync with a specific npm registry if provided with one', async () => {
     const mockRoot = 'libs/my-domain/foo';
-    const context = {
-    } as any;
+    const context = {} as any;
     const registry = 'specific-npm-registry.org';
     process.env = Object.assign(process.env, { NPM_REGISTRY: registry });
 
@@ -44,7 +42,25 @@ describe('NpmPublish Executor', () => {
     /* eslint-disable */
     jest.spyOn(projectHelpers, 'getRoot').mockReturnValue(mockRoot);
 
-    const expectedCommand = `cd ./dist/${mockRoot} && echo '//${registry}/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish`
+    const expectedCommand = `cd ./dist/${mockRoot} && echo '//${registry}/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish --tag=latest`;
+    const output = await executor({}, context);
+
+    expect(child_process.execSync).toHaveBeenCalledWith(expectedCommand);
+    expect(output.success).toBe(true);
+  });
+
+  it('should execSync with a specific npm channel if provided with one', async () => {
+    const mockRoot = 'libs/my-domain/foo';
+    const context = {} as any;
+    const channel = 'beta';
+    process.env = Object.assign(process.env, { CHANNEL: channel });
+
+    /* eslint-disable */
+    jest.spyOn(child_process, 'execSync').mockImplementation((() => {}) as any);
+    /* eslint-disable */
+    jest.spyOn(projectHelpers, 'getRoot').mockReturnValue(mockRoot);
+
+    const expectedCommand = `cd ./dist/${mockRoot} && echo '//registry.npmjs.org/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish --tag=${channel}`;
     const output = await executor({}, context);
 
     expect(child_process.execSync).toHaveBeenCalledWith(expectedCommand);
--- libs/nx-release/src/executors/npm-publish/executor.ts
+++ libs/nx-release/src/executors/npm-publish/executor.ts
@@ -1,17 +1,19 @@
-import {execSync} from 'child_process';
-import {ExecutorContext} from "@nx/devkit";
+import { execSync } from 'child_process';
+import { ExecutorContext } from '@nx/devkit';
 
-import {getRoot} from "../helpers/projects.helpers";
+import { getRoot } from '../helpers/projects.helpers';
 
-import {NpmPublishExecutorSchema} from './schema';
+import { NpmPublishExecutorSchema } from './schema';
 
-export default async function runExecutor(options: NpmPublishExecutorSchema,
-                                          context: ExecutorContext
+export default async function runExecutor(
+  options: NpmPublishExecutorSchema,
+  context: ExecutorContext
 ) {
   const sourceRoot = `./dist/${getRoot(context)}`;
-  const registry: string = process.env.NPM_REGISTRY || 'registry.npmjs.org'
+  const registry: string = process.env.NPM_REGISTRY || 'registry.npmjs.org';
+  const channel: string = process.env.CHANNEL || 'latest';
   execSync(
-    `cd ${sourceRoot} && echo '//${registry}/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish`
+    `cd ${sourceRoot} && echo '//${registry}/:_authToken=${process.env.NPM_TOKEN}' >> .npmrc && npm publish --tag=${channel}`
   );
   return {
     success: true,
